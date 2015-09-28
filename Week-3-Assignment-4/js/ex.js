5f06a46066e63a0e971e4fcf357d0c0cb6ce6d01
alve0024 <alve0024@algonquinlive.com>
2015-09-28 19:27:18 -0400

    The initial code implemented

--- Week-3-Assignment-4/js/ex.js
+++ Week-3-Assignment-4/js/ex.js
@@ -1,3 +1,26 @@
 // JavaScript Document
+// Global variables
+var longRect, shortRect = null;
+
+
+document.addEventListener("DOMContentLoaded", function(){
+    longRect = document.querySelector(".long-rect");
+    longRect.addEventListener("mouseover", mouseOver);
+    longRect.addEventListener("mouseout", mouseOut);
+    shortRect = document.querySelector(".short-rect"); 
+});
+
+function mouseClick() {
+    alert("You clicked!");
+}
+
+function mouseOut() { 
+  shortRect.onclick = mouseClick;
+  shortRect.innerHTML = "The mouse is out the big Box";   
+}
+
+function mouseOver() {
+  shortRect.innerHTML = "The mouse is over the big Box"; 
+}
 
 
